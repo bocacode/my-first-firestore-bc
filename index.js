62e5560bc82b8df02825e9bd30d4220823087453
Todd Albert <todd@bocacode.com>
2022-04-29 11:16:33 -0400

    created first restaurant

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("path/to/serviceAccountKey.json");
+const serviceAccount = require('./credentials.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
+const db = admin.firestore();
+
+const restaurant = {
+  name: 'Mister 01',
+  address: '555 N Federal Hwy Suite #5, Boca Raton, FL 33432',
+  cuisine: 'Pizza',
+  rating: 4.9,
+  phone: '(786) 677-2903',
+}
+
+db.collection('restaurants').add(restaurant)
+  .then(doc => console.log('Created restaurant', doc.id))
+  .catch(err => console.error(err))
